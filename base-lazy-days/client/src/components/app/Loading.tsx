21687075602bcbceca4081c3cabd4af936c0be2f
Hayeon LEE <fhdodl@naver.com>
2024-06-09 21:49:59 +0900

    섹션4 : useIsFetching 훅 사용하여 로딩 상태 처리

---
+++ base-lazy-days/client/src/components/app/Loading.tsx
@@ -0,0 +1,28 @@
+import { Spinner, Text } from "@chakra-ui/react";
+import { useIsFetching } from "@tanstack/react-query";
+export function Loading() {
+  //  useIsFetching 훅을 사용하여 표시할지 안할지 여부를 결정해야 한다.
+  const isFetching = useIsFetching();
+  console.log(isFetching);
+  //useIsFetching의 반환은 현재 가져오기 상태인 쿼리 호춯의 수를 정수로 반환한다.
+
+  const display = isFetching ? "inherit" : "none";
+
+  return (
+    <Spinner
+      thickness="4px"
+      speed="0.65s"
+      emptyColor="olive.200"
+      color="olive.800"
+      role="status"
+      position="fixed"
+      zIndex="9999"
+      top="50%"
+      left="50%"
+      transform="translate(-50%, -50%)"
+      display={display}
+    >
+      <Text display="none">Loading...</Text>
+    </Spinner>
+  );
+}
